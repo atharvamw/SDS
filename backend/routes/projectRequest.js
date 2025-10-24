db407cc147d33eb8fa66969e36549eaf93562851
Atharva Wadekar <156837369+atharvamw@users.noreply.github.com>
2025-10-24 14:41:42 +0530

    SDS Web 1.5 (Added Controllers)

--- backend/routes/projectRequest.js
+++ backend/routes/projectRequest.js
@@ -1,80 +1,14 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-import {createProjectRequest, getProjectRequest, approveProjectRequest, deleteProjectRequest} from "../models/projectRequest.js"
+import { handleRequestProject, handleGetProjectRequest, handleApproveProjectRequest, handleDeleteProject } from '../controllers/projectRequest.js';
 
 const router = express.Router()
-router.post("/requestProject", async (req, res) => {
-    try {
-      const { name, email, title, description } = req.body;
-  
-      if (!name || !email || !title || !description)
-        return res.status(400).json({ status: "failed", message: "All fields required" });
-      
-      const result = await createProjectRequest(name,email, title, description)
-      res.status(201).json(result);
-  
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ status: "error", message: "Server error" });
-    }
-  });
 
-  router.get("/getProjectRequests", async (req, res) => {
+router.post("/requestProject", handleRequestProject);
 
-    try{
-      if(req.cookies.token && jwt.verify(req.cookies.token, process.env.JWT_SECRET))
-      {
-        const projRequests = await getProjectRequest()
-        res.status(200).json(projRequests)
-      }
-      else
-      {
-        res.status(401).json({"status": "failed", "message": "Please Login with a valid account!"})
-      }
-    }
-    catch(err)
-    {
-      res.status(500).json({status: "error", message: err})
-    }
+router.get("/getProjectRequests", handleGetProjectRequest)
 
-  })
+router.post("/approveProjectRequest", handleApproveProjectRequest)
 
-  router.post("/approveProjectRequest", async (req, res) => {
-
-    try{
-      if(req.cookies.token && req.body.id && jwt.verify(req.cookies.token, process.env.JWT_SECRET))
-      {
-        const projRequests = await approveProjectRequest(req.body.id)
-        if(projRequests.status=="success")
-          res.status(200).json(projRequests)
-        else
-          res.status(500).json(projRequests)
-      }
-      else if(!req.body.id)
-      {
-        res.status(404).json({"status": "failed", "message": "Please Enter a valid project id"})
-      }
-      else
-      {
-        res.status(401).json({"status": "failed", "message": "Please Login with a valid account!"})
-      }
-    }
-    catch(err)
-    {
-      res.json({status: "error", message: err})
-    }
-
-  })
-
-  router.post("/deleteProjectRequest", async (req,res)=>{
-
-    if(req.cookies.token && jwt.verify(req.cookies.token,process.env.JWT_SECRET))
-    {
-        const result = await deleteProjectRequest(req.body.id)
-        res.json(result);
-    }    
-    else
-        res.json({"authentication": "failed", "message": "You must be Logged in as an Admin!"});
-})
+router.post("/deleteProjectRequest", handleDeleteProject)
 
   export default router
--- backend/routes/team.js
+++ backend/routes/team.js
@@ -1,20 +1,8 @@
 import express from 'express'
-import { getAllTeam } from '../models/team.js'
+import { handleGetTeam } from '../controllers/team.js';
 
 const router = express.Router()
 
-router.get("/getTeam", async (req, res) => {
-    try {
-      const team = await getAllTeam();
-  
-      res.status(200).json({
-        status: "success",
-        team: team
-      });
-    } catch (error) {
-      console.error("Error in /getTeam route:", error);
-      res.status(500).json({ status: "error", message: "Failed to retrieve team data." });
-    }
-  });
+router.get("/getTeam", handleGetTeam);
 
 export default router
